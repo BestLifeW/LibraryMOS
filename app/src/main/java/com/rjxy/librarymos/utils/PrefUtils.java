85c96cb4574bbe8990f2d4fea5d4eb1a7916e4fc
linliantong <478315414@qq.com>
2016-10-28 21:45:20 +0800

    lt的测试

--- app/src/main/java/com/rjxy/librarymos/utils/PrefUtils.java
+++ app/src/main/java/com/rjxy/librarymos/utils/PrefUtils.java
@@ -1,7 +1,14 @@
 package com.rjxy.librarymos.utils;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
 
 /**
  * Created by lovec on 2016/9/19.
@@ -39,5 +46,22 @@ public class PrefUtils {
     public static void setInt(Context context, String key, int value) {
         SharedPreferences sp = context.getSharedPreferences("config", Context.MODE_PRIVATE);
         sp.edit().putInt(key, value).apply();
+    }
+
+    public static byte[] bmpToByteArray(Bitmap photo){
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        try{
+            photo.compress(Bitmap.CompressFormat.PNG,100,bos);
+            bos.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return bos.toByteArray();
+    }
+
+    public static void getImageFromAlbum(Activity activity){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        activity.startActivityForResult(intent,0);
     }
 }
