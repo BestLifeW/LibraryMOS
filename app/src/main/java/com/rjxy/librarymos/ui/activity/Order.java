ef69bbaa0d0b95480359cb33dbd82d0b077431dc
panzc15 <707658359@qq.com>
2016-12-08 17:41:58 +0800

    orderUI

---
+++ app/src/main/java/com/rjxy/librarymos/ui/activity/Order.java
@@ -0,0 +1,15 @@
+package com.rjxy.librarymos.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.rjxy.librarymos.R;
+
+public class Order extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
